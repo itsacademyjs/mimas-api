e429b43c4de826e03bc5b00c9391412f79214903
Samuel Rowe <samuelrowe1999@gmail.com>
2021-10-06 16:58:39 +0530

    chore: migrated app/model/index.js to TypeScript

---
+++ app/model/index.ts
@@ -0,0 +1,6 @@
+export { default as Article } from "./article";
+export { default as Chapter } from "./chapter";
+export { default as Course } from "./course";
+export { default as Section } from "./section";
+export { default as User } from "./user";
+export { default as TestSuite } from "./test-suite";
