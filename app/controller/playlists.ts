202cb19522cbf3ca95a6a0065e4eee73e78ce73d
Samuel Rowe <samuelrowe1999@gmail.com>
2021-10-24 10:56:27 +0530

    feat: created the `update` controler for playlists

--- app/controller/playlists.ts
+++ app/controller/playlists.ts
@@ -2,7 +2,7 @@ import joi from "joi";
 
 import { Playlist } from "../model";
 import { identifierPattern } from "../util/constants";
-import { BadRequestError } from "../util";
+import { BadRequestError, NotFoundError } from "../util";
 
 const toExternal = (playlist) => {
     const { _id, title, description, courses, status } = playlist;
@@ -24,6 +24,12 @@ const createSchema = joi.object({
         .default([]),
 });
 
+const updateSchema = joi.object({
+    title: joi.string().max(512).allow(""),
+    description: joi.string().max(1024).allow(""),
+    courses: joi.array().items(joi.string().regex(identifierPattern)),
+});
+
 const create = async (context, attributes) => {
     const { error, value } = createSchema.validate(attributes, {
         stripUnknown: true,
@@ -43,4 +49,42 @@ const create = async (context, attributes) => {
     return toExternal(newPlaylist);
 };
 
-export { create };
+const update = async (context, playlistId, attributes) => {
+    if (!identifierPattern.test(playlistId)) {
+        throw new BadRequestError(
+            "The specified playlist identifier is invalid."
+        );
+    }
+
+    const { error, value } = updateSchema.validate(attributes, {
+        stripUnknown: true,
+    });
+    if (error) {
+        throw new BadRequestError(error.message);
+    }
+
+    const playlist = await Playlist.findOneAndUpdate(
+        {
+            _id: playlistId,
+            creator: context.user._id,
+            status: {
+                $ne: "deleted",
+            },
+        },
+        value,
+        {
+            new: true,
+            lean: true,
+        }
+    ).exec();
+
+    if (!playlist) {
+        throw new NotFoundError(
+            "A playlist with the specified identifier does not exist."
+        );
+    }
+
+    return toExternal(playlist);
+};
+
+export { create, update };
